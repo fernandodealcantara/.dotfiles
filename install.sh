62e5330005b13f653712495eb93d25b9b847d6f8
Fernando de Alcantara <45339608+fernandodealcantara@users.noreply.github.com>
2022-11-12 19:59:41 -0300

    updating

--- install.sh
+++ install.sh
@@ -2,27 +2,27 @@
 ## Install script for the dotfiles
 
 ## functions
-function upgrade(){
+function upgrade() {
     sudo apt update && sudo apt upgrade -y
 }
 
-function addRepos(){
+function addRepos() {
     #nvim
     sudo add-apt-repository ppa:neovim-ppa/stable -y
 }
 
-function installPackages(){
+function installPackages() {
     # Apt packages
     sudo apt install -y \
-    software-properties-common \
-    make build-essential libssl-dev zlib1g-dev \
-    libbz2-dev libreadline-dev libsqlite3-dev wget llvm \
-    libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev \
-    curl git stow zsh bat zip unzip ripgrep fd-find fzf neovim tmux \
-    default-jre default-jdk exa nala wslu
+        software-properties-common \
+        make build-essential libssl-dev zlib1g-dev \
+        libbz2-dev libreadline-dev libsqlite3-dev wget llvm \
+        libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev \
+        curl git stow zsh bat zip unzip ripgrep fd-find fzf neovim tmux \
+        default-jre default-jdk exa nala wslu
 }
 
-function symLink(){
+function symLink() {
     # Symlink dotfiles
     stow -v -d ~/.dotfiles/ -t ~ zsh
     stow -v -d ~/.dotfiles/ -t ~ fonts
@@ -30,22 +30,20 @@ function symLink(){
     stow -v -d ~/.dotfiles/ -t ~ git
 }
 
-function copyWindowsConfigFiles(){
-    mkdir "$(wslpath $(wslvar -l AppData))/alacritty"
-    cp ~/.dotfiles/alacritty/alacritty.yml "$(wslpath $(wslvar -l AppData))/alacritty/"
-    cp ~/.dotfiles/wslconfig/.wslconfig $(wslpath $(wslvar -s USERPROFILE))
+function copyWindowsConfigFiles() {
+    cp ~/.dotfiles/wslconfig/.wslconfig "$(wslpath "$(wslvar -s USERPROFILE)")"
 }
 
-function gitSetup(){
+function gitSetup() {
     # Git config
-    git config --global user.name $USER_NAME
-    git config --global user.email $USER_EMAIL
-    git config --global commit.template $GIT_COMMIT_TEMPLATE
+    git config --global commit.template "$GIT_COMMIT_TEMPLATE"
+    git config --global user.name "$USER_NAME"
+    git config --global user.email "$USER_EMAIL"
 }
 
-function getClipboardTool(){
+function getClipboardTool() {
     wget https://github.com/equalsraf/win32yank/releases/download/v0.0.4/win32yank-x64.zip -O /tmp/win32yank.zip
-    unzip /tmp/win32yank.zip win32yank.exe -d $(wslpath $(wslvar -s USERPROFILE))
+    unzip /tmp/win32yank.zip win32yank.exe -d "$(wslpath "$(wslvar -s USERPROFILE)")"
 }
 
 upgrade
@@ -64,4 +62,3 @@ source ~/.zshrc
 
 gitSetup
 # programming languages
-
